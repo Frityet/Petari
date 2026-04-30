5896c61304e6f5789701ada68e2bc910c3ffab6b
Fl-ppie <sebastiaan.aloisius@gmail.com>
2026-04-30 06:40:46 +0200

    GrandStarReturnDemoStarter

--- include/Game/Demo/ReturnDemoRailMove.hpp
+++ include/Game/Demo/ReturnDemoRailMove.hpp
@@ -1,30 +1,31 @@
 #pragma once
 
+#include "Game/LiveActor/LiveActor.hpp"
+#include "Game/MapObj/SpinDriverPathDrawer.hpp"
+#include "Game/MapObj/SpinDriverShootPath.hpp"
+#include "Game/Util/JMapInfo.hpp"
 #include "JSystem/JGeometry/TMatrix.hpp"
-
-class LiveActor;
-class JMapInfoIter;
-class SpinDriverShootPath;
-class SpinDriverPathDrawer;
+#include "JSystem/JGeometry/TVec.hpp"
 
 class ReturnDemoRailMove {
 public:
-    ReturnDemoRailMove(LiveActor*, LiveActor*, const JMapInfoIter&, bool, TPos3f*);
+    ReturnDemoRailMove(LiveActor*, LiveActor*,
+                       const JMapInfoIter&, bool, TPos3f*);
 
     void posToStart();
     void posToEnd();
     void offPathDraw();
     s32 getDemoFlyBrakeFrame() const;
-    void calcPathPosDir(TVec3f*, TVec3f*, f32) const;
+    void calcPathPosDir(TVec3f*, TVec3f*, float) const;
     void setupPathDrawForGraneStarReturnDemo();
     void start();
-    void update(s32, s32);
+    void update(long, long);
 
-    /* 0x0 */ LiveActor* mDemoStarter;  // Can be either StarReturnDemoStarter or GrandStarReturnDemoStarter
-    /* 0x4 */ LiveActor* mPowerStar;
-    /* 0x8 */ bool mIsGrandStar;
-    /* 0xC */ TPos3f* mTransform;
-    /* 0x10 */ SpinDriverShootPath* mShootPath;
-    /* 0x14 */ SpinDriverPathDrawer* mPathDrawer;
-    /* 0x18 */ TVec3f mForward;
+    /* 0x0 */ LiveActor* demoStarter;
+    /* 0x4 */ LiveActor* powerStar;
+    /* 0x8 */ bool isGrandStar;
+    /* 0xC */ TPos3f* transform;
+    /* 0x10 */ SpinDriverShootPath* shootPath;
+    /* 0x14 */ SpinDriverPathDrawer* pathDrawer;
+    /* 0x18 */ TVec3f forward;
 };
