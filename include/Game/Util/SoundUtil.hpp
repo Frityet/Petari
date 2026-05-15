57e4a045b47a8b24160aac0611e0aebe283187ac
Fl-ppie <sebastiaan.aloisius@gmail.com>
2026-05-15 23:57:47 +0200

    Reapply "Header files"
    This reverts commit 8757a8cbb8a427f08d6a6b362bc0415c1980181d.

--- include/Game/Util/SoundUtil.hpp
+++ include/Game/Util/SoundUtil.hpp
@@ -20,6 +20,7 @@ namespace MR {
     void stopStageBGM(u32);
     void limitedSound(const char*, s32);
     void unlockStageBGM();
+    void setNextStageBGM(const char*);
 
     void startLastStageBGM();
 
