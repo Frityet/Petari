6f0305313af1a31bcfd8929e7c5b38e5fcd393b9
Amrit Bhogal <ambhogal01@gmail.com>
2026-05-17 13:52:15 +0000

    docker fix

--- pc-port/container/entrypoint.sh
+++ pc-port/container/entrypoint.sh
@@ -15,6 +15,9 @@ Start an interactive shell:
 Start an interactive shell with this repo mounted read/write:
   podman run --rm -it --userns=keep-id --security-opt label=disable -v "$PWD:/workspaces/pcport/pc-port" pc-port:podman-test
 
+Run smg-pc with X11 display access:
+  podman run --rm --userns=keep-id --ipc=host --security-opt label=disable -e DISPLAY -e XAUTHORITY=/tmp/.container-xauth -v "$PWD:/workspaces/pcport/pc-port" -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v "$XAUTHORITY:/tmp/.container-xauth:ro" pc-port:podman-test xmake run smg-pc
+
 Run a command:
   podman run --rm pc-port:podman-test xmake --version
 EOF
