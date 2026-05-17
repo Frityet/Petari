e7b629f47d53c3bd5c3b427ec435a4f4d661e387
Amrit Bhogal <ambhogal01@gmail.com>
2026-05-17 11:45:56 +0000

    matych

--- configure.py
+++ configure.py
@@ -1027,7 +1027,7 @@ config.libs = [
             Object(Matching, "Game/Demo/DemoCastSubGroup.cpp"),
             Object(Matching, "Game/Demo/DemoCtrlBase.cpp"),
             Object(NonMatching, "Game/Demo/DemoDirector.cpp"),
-            Object(NonMatching, "Game/Demo/DemoExecutor.cpp"),
+            Object(Matching, "Game/Demo/DemoExecutor.cpp"),
             Object(Matching, "Game/Demo/DemoExecutorFunction.cpp"),
             Object(Matching, "Game/Demo/DemoFunction.cpp"),
             Object(NonMatching, "Game/Demo/DemoKoopaJrShip.cpp"),
